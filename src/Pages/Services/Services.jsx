02cd08f26c24841a80713b294ec44ecd4c421582
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-04-01 19:15:01 +0600

    added the card 3

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -13,10 +13,10 @@ const Services = () => {
         {
             id: 2,
             title: "Luxury Rentals",
-//             desc: "Access an exclusive collection of high-end apartments and villas for short or long-term stays.",
-//             icon: <Key className="w-8 h-8 text-orange-600" />,
-//             bgColor: "bg-orange-50 dark:bg-orange-900/20"
-//         },
+            desc: "Access an exclusive collection of high-end apartments and villas for short or long-term stays.",
+            icon: <Key className="w-8 h-8 text-orange-600" />,
+            bgColor: "bg-orange-50 dark:bg-orange-900/20"
+        },
 //         {
 //             id: 3,
 //             title: "Property Management",
