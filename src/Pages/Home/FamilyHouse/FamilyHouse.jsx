81ef55ff6d9abd52aa9efc17c53109cbd8844a85
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-04-04 19:58:33 +0600

    added the button

--- src/Pages/Home/FamilyHouse/FamilyHouse.jsx
+++ src/Pages/Home/FamilyHouse/FamilyHouse.jsx
@@ -126,9 +126,9 @@ const FamilyHouse = () => {
                     className="p-2 bg-black/50 hover:bg-black/70 backdrop-blur-md rounded-full text-white transition-all border border-white/20"
                      title="Zoom In"
                    >
-//                     <Plus size={16} />
-//                   </button>
-//                   <button 
+                     <Plus size={16} />
+                   </button>
+                   <button 
 //                     onClick={() => handleZoomOut(house.id)}
 //                     className="p-2 bg-black/50 hover:bg-black/70 backdrop-blur-md rounded-full text-white transition-all border border-white/20"
 //                     title="Zoom Out"
