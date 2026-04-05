040c28885bed9da0d4fe56ba84dbc93a850a19c9
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-04-05 22:03:27 +0600

    added the apartment card 1

--- src/Pages/Home/Apartment/Apartment.jsx
+++ src/Pages/Home/Apartment/Apartment.jsx
@@ -17,17 +17,17 @@ const Apartment = () => {
       floor: "12th Floor",
       image: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?auto=format&fit=crop&q=80&w=1000"
     },
-//     {
-//       id: 2,
-//       title: "Urban Comfort Suite",
-//       location: "Banani, Dhaka",
-//       price: "$275,000",
-//       beds: 3,
-//       baths: 2,
-//       size: "1600",
-//       floor: "5th Floor",
-//       image: "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?auto=format&fit=crop&q=80&w=1000"
-//     },
+    {
+      id: 2,
+      title: "Urban Comfort Suite",
+      location: "Banani, Dhaka",
+      price: "$275,000",
+      beds: 3,
+      baths: 2,
+      size: "1600",
+      floor: "5th Floor",
+      image: "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?auto=format&fit=crop&q=80&w=1000"
+    },
 //     {
 //       id: 3,
 //       title: "Panoramic View Penthouse",
