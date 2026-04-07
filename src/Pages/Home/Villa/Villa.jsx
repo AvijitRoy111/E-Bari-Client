80dce206c7f9eec237f7d76e66eb27aa3551f7d4
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-04-07 12:25:50 +0600

    added the villa card 1

--- src/Pages/Home/Villa/Villa.jsx
+++ src/Pages/Home/Villa/Villa.jsx
@@ -11,9 +11,9 @@ const Villa = () => {
       title: "Royal Azure Villa",
       location: "Malibu, California",
       price: "$1,250,000",
-    //   beds: 6,
-    //   baths: 5,
-    //   size: "4500",
+      beds: 6,
+      baths: 5,
+      size: "4500",
     //   image: "https://images.unsplash.com/photo-1613490493576-7fde63acd811?auto=format&fit=crop&q=80&w=1000"
     // },
 //     {
