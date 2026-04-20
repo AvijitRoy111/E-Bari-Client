129b91931b52a9b15cdcc7b29d6c6d7090d22526
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-04-20 20:56:19 +0600

    added the password

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -129,8 +129,8 @@ const SignUp = () => {
                   onClick={() => setShowConfirmPassword(!showConfirmPassword)}
                   className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-400 hover:text-sky-600"
                  >
-//                   {showConfirmPassword ? <EyeOff size={19} /> : <Eye size={19} />}
-//                 </button>
+                   {showConfirmPassword ? <EyeOff size={19} /> : <Eye size={19} />}
+                 </button>
 //               </div>
 //             </div>
 
