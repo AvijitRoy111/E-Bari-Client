7d5df1b2f503361a17e319393931b834e07859d8
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-04-20 20:45:23 +0600

    added the signup page

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,6 +1,6 @@
-// import React, { useState } from 'react';
-// import { Eye, EyeOff, Mail, Lock, User, ArrowRight, Phone } from 'lucide-react';
-// import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Eye, EyeOff, Mail, Lock, User, ArrowRight, Phone } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 // const SignUp = () => {
 //   const [showPassword, setShowPassword] = useState(false);
