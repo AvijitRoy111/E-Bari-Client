d5e249952503467c366cec2d2dcc4468890a0d67
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-04-21 15:15:01 +0600

    added the text

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom';
 const SignIn = () => {
   const [showPassword, setShowPassword] = useState(false);
 
-//   return (
-//     // Parent container with dark mode background
-//     <div className="min-h-screen w-full flex items-center justify-center p-4 font-sans bg-slate-50 dark:bg-slate-950 transition-colors duration-300">
+  return (
+    // Parent container with dark mode background
+    <div className="min-h-screen w-full flex items-center justify-center p-4 font-sans bg-slate-50 dark:bg-slate-950 transition-colors duration-300">
       
 //       {/* Main Card */}
 //       <div className="max-w-4xl w-full bg-white dark:bg-slate-900 rounded-[2.5rem] shadow-2xl overflow-hidden flex flex-col md:flex-row border border-transparent dark:border-slate-800">
