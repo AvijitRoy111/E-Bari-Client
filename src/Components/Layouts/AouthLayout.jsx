339dadf8263e2aef5c068bd8f62e1d11e16f0db0
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-04-22 23:39:12 +0600

    added the section

--- src/Components/Layouts/AouthLayout.jsx
+++ src/Components/Layouts/AouthLayout.jsx
@@ -3,7 +3,7 @@ import Navbar from '../Navbar/Navbar'
 import { Outlet } from 'react-router-dom'
 
 const AouthLayout = () => {
-//   return (
+  return (
 
 //     <div className="flex flex-col min-h-screen ">
 //       <Navbar />
