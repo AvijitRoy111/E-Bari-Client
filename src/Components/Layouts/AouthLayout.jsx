bc3fc2ca9a4ded7b3b5da1375b69a73bd9f43086
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-04-22 23:39:47 +0600

    added the class

--- src/Components/Layouts/AouthLayout.jsx
+++ src/Components/Layouts/AouthLayout.jsx
@@ -7,7 +7,7 @@ const AouthLayout = () => {
 
     <div className="flex flex-col min-h-screen ">
        <Navbar />
-//       <div className="flex flex-grow">
+       <div className="flex flex-grow">
 //         <Outlet></Outlet>
 //       </div>
 
