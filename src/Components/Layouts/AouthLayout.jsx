5d5e0fc3cac601ad0ebbaf6561de011242dd6fe5
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-04-22 23:39:25 +0600

    added the return

--- src/Components/Layouts/AouthLayout.jsx
+++ src/Components/Layouts/AouthLayout.jsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom'
 const AouthLayout = () => {
   return (
 
-//     <div className="flex flex-col min-h-screen ">
+    <div className="flex flex-col min-h-screen ">
 //       <Navbar />
 //       <div className="flex flex-grow">
 //         <Outlet></Outlet>
