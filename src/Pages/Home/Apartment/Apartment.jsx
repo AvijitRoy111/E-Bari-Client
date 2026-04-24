3fe4eed69c75f8d34a40bec1c5fd52aac27895bd
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-04-24 21:15:16 +0600

    added the apartment

--- src/Pages/Home/Apartment/Apartment.jsx
+++ src/Pages/Home/Apartment/Apartment.jsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import { Bed, Bath, Maximize, MapPin, ArrowLeft, Heart, Plus, Minus, Building2 } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
-// const Apartment = () => {
-//   const navigate = useNavigate();
+const Apartment = () => {
+  const navigate = useNavigate();
 
-//   const apartmentList = [
+  const apartmentList = [
 //     {
 //       id: 1,
 //       title: "Skyline Luxury Apartment",
