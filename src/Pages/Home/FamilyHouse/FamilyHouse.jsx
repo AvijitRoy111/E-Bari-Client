00bfdcfda9311438841d24a0fafd6873654ca723
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-04-25 17:21:29 +0600

    added the familyhouse zoom in

--- src/Pages/Home/FamilyHouse/FamilyHouse.jsx
+++ src/Pages/Home/FamilyHouse/FamilyHouse.jsx
@@ -68,9 +68,9 @@ const FamilyHouse = () => {
     }));
   };
 
-//   const handleZoomOut = (id) => {
-//     setZoomLevels(prev => ({
-//       ...prev,
+  const handleZoomOut = (id) => {
+    setZoomLevels(prev => ({
+      ...prev,
 //       [id]: Math.max((prev[id] || 1) - 0.2, 1) // Minimum zoom level is 1
 //     }));
 //   };
