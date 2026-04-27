4c11b7edfd0e421e6bc9e444457a74407774c630
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-04-27 18:09:59 +0600

    added the use consr

--- src/hooks/useProperties.jsx
+++ src/hooks/useProperties.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axiosPublic from "../api/axiosPublic";
 
 const useProperties = () => {
-//     const [properties, setProperties] = useState([]);
+    const [properties, setProperties] = useState([]);
 //     const [loading, setLoading] = useState(true);
 
 //     useEffect(() => {
