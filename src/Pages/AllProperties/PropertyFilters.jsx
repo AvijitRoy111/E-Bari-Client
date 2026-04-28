72ea16c0705dfec8dd0bf687f646adf4c2b9957c
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-04-28 21:16:23 +0600

    added the tab

--- src/Pages/AllProperties/PropertyFilters.jsx
+++ src/Pages/AllProperties/PropertyFilters.jsx
@@ -51,9 +51,9 @@ const PropertyFilters = ({ loading, searchTerm, setSearchTerm, activeTab, setAct
                  />
               </div>
 
-//             {/* Category Tabs */}
-//              <div className="flex bg-gray-100 dark:bg-gray-700 p-1.5 rounded-xl">
-//                 {["All", "Villa", "Apartment"].map((type) => (
+             {/* Category Tabs */}
+              <div className="flex bg-gray-100 dark:bg-gray-700 p-1.5 rounded-xl">
+                 {["All", "Villa", "Apartment"].map((type) => (
 //                      <button
 //                          key={type}
 //                          onClick={() => setActiveTab(type)}
