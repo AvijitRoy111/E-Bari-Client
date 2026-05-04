b3d5029b60eeed7ff4a5a39b57ad76378292ea89
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-04 16:34:29 +0600

    added the name

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -31,8 +31,8 @@ const Contact = () => {
                    <Mail className="w-6 h-6 text-blue-600" />
                  </div>
                  <div>
-//                   <p className="text-sm text-gray-500 dark:text-gray-400">Email us at</p>
-//                   <p className="text-lg font-semibold text-gray-900 dark:text-white">support@dreamhome.com</p>
+                   <p className="text-sm text-gray-500 dark:text-gray-400">Email us at</p>
+                   <p className="text-lg font-semibold text-gray-900 dark:text-white">support@dreamhome.com</p>
 //                 </div>
 //               </div>
 
