7cd5311b2bd063ab9576f861daddb7ab9592bbdc
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-07 18:40:52 +0600

    added the toaster

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -6,9 +6,9 @@ export const AuthContext = createContext(null);
 
 // Configure SweetAlert Toast
 const Toast = Swal.mixin({
-//   toast: true,
-//   position: "top-end",
-//   showConfirmButton: false,
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
 //   timer: 3000,
 //   timerProgressBar: true,
 //   didOpen: (toast) => {
