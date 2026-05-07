3d6c0d9d7af634ca30c7d8031842b37eb50b358d
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-07 18:40:11 +0600

    added the authcontext

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -2,7 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import axiosPublic from "@/api/axiosPublic";
 import Swal from "sweetalert2";
 
-// export const AuthContext = createContext(null);
+export const AuthContext = createContext(null);
 
 // // Configure SweetAlert Toast
 // const Toast = Swal.mixin({
