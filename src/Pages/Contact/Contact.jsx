7d2f57a0a3d1770ce5b11b8f0547498d99030da2
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-10 15:26:39 +0600

    added the button

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -48,7 +48,7 @@ const Contact = () => {
     const handleCloseModal = () => {
         setShowModal(false);
         setFormData({ name: '', email: '', phone: '', subject: '', message: '' });
-//     };
+    };
 
 //     return (
 //         <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors duration-300 relative">
