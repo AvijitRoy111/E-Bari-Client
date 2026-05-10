1a1b0ff69920c83be4c910cd78698caf5b0bb834
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-10 15:20:49 +0600

    added the contact

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 // import { 
 //     Mail, Phone, MapPin, Send, MessageSquare, 
 //     CheckCircle, Clock, Facebook, Twitter, Instagram, Linkedin 
