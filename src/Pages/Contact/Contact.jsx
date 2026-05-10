28bf975b71f104446f7f03b0d81e6a8ddce12ba0
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-10 15:21:23 +0600

    added the contact mail

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { 
     Mail, Phone, MapPin, Send, MessageSquare, 
-//     CheckCircle, Clock, Facebook, Twitter, Instagram, Linkedin 
+    CheckCircle, Clock, Facebook, Twitter, Instagram, Linkedin 
 // } from 'lucide-react';
 // import Breadcrumb from "@/Components/Breadcrumb/Breadcrumb";
 // import axiosPublic from '@/api/axiosPublic';
