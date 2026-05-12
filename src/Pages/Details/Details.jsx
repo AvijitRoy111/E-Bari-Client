0d6bac494624709763d12f5e26bd8fdff30d2a7f
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-12 11:18:31 +0600

    added the id

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -148,9 +148,9 @@ const Details = () => {
              </div>
 
              {property.images?.length > 1 && (
-//               <div className="flex gap-3 overflow-x-auto pb-2 no-scrollbar px-1">
-//                 {property.images.map((img, idx) => (
-//                   <button key={idx} onClick={() => {setActiveImage(idx); setZoom(1);}} className={`relative min-w-[80px] md:min-w-[120px] h-16 md:h-24 rounded-2xl overflow-hidden border-4 transition-all ${activeImage === idx ? "border-blue-600 scale-105" : "border-transparent opacity-60"}`}>
+               <div className="flex gap-3 overflow-x-auto pb-2 no-scrollbar px-1">
+                 {property.images.map((img, idx) => (
+                   <button key={idx} onClick={() => {setActiveImage(idx); setZoom(1);}} className={`relative min-w-[80px] md:min-w-[120px] h-16 md:h-24 rounded-2xl overflow-hidden border-4 transition-all ${activeImage === idx ? "border-blue-600 scale-105" : "border-transparent opacity-60"}`}>
 //                     <img src={img} className="w-full h-full object-cover" alt="thumb" />
 //                   </button>
 //                 ))}
