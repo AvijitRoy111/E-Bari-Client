6b007a7973b36cbd921b3a3833e69d57e8bff85a
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-15 00:11:14 +0600

    added the favourite text

--- src/Pages/Favourite/Favourite.jsx
+++ src/Pages/Favourite/Favourite.jsx
@@ -1,6 +1,6 @@
-// import React, { useState, useEffect } from 'react';
-// import { MapPin, BedDouble, Bath, Square, Heart, Trash2 } from 'lucide-react';
-// import Breadcrumb from '@/Components/Breadcrumb/Breadcrumb';
+import React, { useState, useEffect } from 'react';
+import { MapPin, BedDouble, Bath, Square, Heart, Trash2 } from 'lucide-react';
+import Breadcrumb from '@/Components/Breadcrumb/Breadcrumb';
 
 // const Favourite = () => {
 //     const [favItems, setFavItems] = useState([]);
