d2471e6d5e07fd32f823637619b6fbb5850299da
AvijitRoy111 <bormonbidhan12@example.com>
2026-05-18 13:12:01 -0700

    added the section

--- src/Components/BrowseHome/BrowseHome.jsx
+++ src/Components/BrowseHome/BrowseHome.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-// const BrowseHome = () => {
-//     const [homes, setHomes] = useState([]);
-//     const [loading, setLoading] = useState(true);
+const BrowseHome = () => {
+    const [homes, setHomes] = useState([]);
+    const [loading, setLoading] = useState(true);
 
 //     // How many items to show
 //     const [visibleCount, setVisibleCount] = useState(3);
