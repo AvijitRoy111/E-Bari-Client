13fec217e5cbd4140678822ebee2d3f56250ce6e
AvijitRoy111 <bormonbidhan12@example.com>
2026-05-18 13:10:41 -0700

    added the import

--- src/Components/BrowseHome/BrowseHome.jsx
+++ src/Components/BrowseHome/BrowseHome.jsx
@@ -1,5 +1,5 @@
-// import { useState, useEffect } from "react";
-// import axios from "axios";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 // const BrowseHome = () => {
 //     const [homes, setHomes] = useState([]);
