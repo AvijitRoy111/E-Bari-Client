d59cef4484d446f90247500f5b7e88ade924d568
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-19 22:05:41 -0700

    added the footer

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -62,9 +62,9 @@ const ErrorPage = () => {
              Home-e Fire Jan
            </a>
          </div>
-//       </main>
+       </main>
 
-//       {/* Footer / Bottom Spacing */}
+       {/* Footer / Bottom Spacing */}
 //       <footer className="py-6 text-center text-sm text-gray-400 border-t border-gray-100 bg-white">
 //         &copy; {new Date().getFullYear()} Your Company. All rights reserved.
 //       </footer>
