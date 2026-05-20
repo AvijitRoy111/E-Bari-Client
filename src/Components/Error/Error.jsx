9cebc86ed893d6d7ba7f1d9aafad66950b862dcc
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-19 22:03:04 -0700

    added the path

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -48,8 +48,8 @@ const ErrorPage = () => {
                fill="none" 
                viewBox="0 0 24 24" 
                stroke="currentColor"
-//             >
-//               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+             >
+               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
 //             </svg>
 //             Ager Page-e Fire Jan
 //           </button>
