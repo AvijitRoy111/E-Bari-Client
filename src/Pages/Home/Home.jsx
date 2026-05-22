bf04ca419e128e6394b9a0a54eef380f427cfa96
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-23 00:24:59 +0600

    added the loading

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -71,7 +71,7 @@ export const Home = () => {
              {/* Navigation Buttons */}
              <div className="flex gap-2">
                <button
-//                 className={`prev-${uniqueSelector} p-2 rounded-full bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800 shadow-sm hover:bg-blue-600 hover:text-white dark:text-white transition-all cursor-pointer`}
+                 className={`prev-${uniqueSelector} p-2 rounded-full bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800 shadow-sm hover:bg-blue-600 hover:text-white dark:text-white transition-all cursor-pointer`}
 //               >
 //                 <ChevronLeft size={20} />
 //               </button>
