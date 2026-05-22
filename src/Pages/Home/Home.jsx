b0b53c7c974cc181f2dbefb4564d6c30e110bd24
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-23 00:26:21 +0600

    added the carosoul

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -78,8 +78,8 @@ export const Home = () => {
 
                <button
                  className={`next-${uniqueSelector} p-2 rounded-full bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800 shadow-sm hover:bg-blue-600 hover:text-white dark:text-white transition-all cursor-pointer`}
-//               >
-//                 <ChevronRight size={20} />
+               >
+                 <ChevronRight size={20} />
 //               </button>
 //             </div>
 
