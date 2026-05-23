745c2087657f0235adc54cfe091112d3a36e135a
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-24 00:04:24 +0600

    added the modal

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -149,7 +149,7 @@ export const Home = () => {
                    <div className="flex justify-between items-center">
                      <span className="text-2xl font-black text-blue-600">
                        $
-//                       {item?.price_min
+                       {item?.price_min
 //                         ? item.price_min.toLocaleString()
 //                         : item?.price?.toLocaleString() || "N/A"}
 //                     </span>
