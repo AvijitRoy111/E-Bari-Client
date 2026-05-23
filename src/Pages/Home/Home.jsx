929141d998f9dbe638741b90a6a17a8c77659c46
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-24 00:06:21 +0600

    added the arrow

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -158,7 +158,7 @@ export const Home = () => {
                        to={`/details/${item?.id}`}
                        className="p-3 bg-gray-100 dark:bg-gray-800 text-gray-700 dark:text-gray-300 rounded-xl hover:bg-blue-600 hover:text-white transition-colors"
                      >
-//                       <ArrowRight size={20} />
+                       <ArrowRight size={20} />
 //                     </Link>
 //                   </div>
 //                 </div>
