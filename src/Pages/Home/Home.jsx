f71ff19aa65a877a6fa95611016a16e0e78c48a8
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-24 00:01:18 +0600

    added the tittle

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -135,7 +135,7 @@ export const Home = () => {
 
                  <div className="p-6">
                    <h3 className="text-xl font-bold dark:text-white line-clamp-1 mb-2">
-//                     {item?.title}
+                     {item?.title}
 //                   </h3>
 
 //                   <div className="flex items-center gap-1 text-gray-400 text-sm mb-4">
