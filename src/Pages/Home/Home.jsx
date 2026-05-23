dc68e0ad2d8e5667d32543b2daecaa7531e26a8f
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-24 00:06:41 +0600

    added the arrow

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -159,8 +159,8 @@ export const Home = () => {
                        className="p-3 bg-gray-100 dark:bg-gray-800 text-gray-700 dark:text-gray-300 rounded-xl hover:bg-blue-600 hover:text-white transition-colors"
                      >
                        <ArrowRight size={20} />
-//                     </Link>
-//                   </div>
+                     </Link>
+                   </div>
 //                 </div>
 //               </div>
 //             </SwiperSlide>
