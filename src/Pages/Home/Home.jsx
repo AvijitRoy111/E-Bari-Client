ef3fc086f3ee401bb511298cf95d7f6d7a498fdc
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-25 15:26:21 +0600

    added the catagory

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -187,7 +187,7 @@ export const Home = () => {
 
          category: PropTypes.string,
 
-//         images: PropTypes.arrayOf(
+         images: PropTypes.arrayOf(
 //           PropTypes.string
 //         ),
 
