19f4101cfaf7f0eff29218e83af94ca3f597c6f8
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-07 15:22:20 +0800

    added the text

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -328,7 +328,7 @@ export const Home = () => {
          </div>
 
          {/* Testimonial */}
-//         <div className="mt-32 bg-blue-600 rounded-[3rem] p-12 md:p-20 relative overflow-hidden text-center">
+         <div className="mt-32 bg-blue-600 rounded-[3rem] p-12 md:p-20 relative overflow-hidden text-center">
 //           <Quote
 //             className="absolute top-10 left-10 text-white/10"
 //             size={120}
