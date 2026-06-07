56b3ee5ca13319af58c41a0c59a93867755e5b7e
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-07 15:23:09 +0800

    added the loading

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -331,7 +331,7 @@ export const Home = () => {
          <div className="mt-32 bg-blue-600 rounded-[3rem] p-12 md:p-20 relative overflow-hidden text-center">
            <Quote
              className="absolute top-10 left-10 text-white/10"
-//             size={120}
+             size={120}
 //           />
 
 //           <h2 className="text-4xl font-black text-white mb-12 relative z-10">
