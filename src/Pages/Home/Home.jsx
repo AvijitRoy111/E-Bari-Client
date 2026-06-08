f76e7db1b87df63bb04b170ee9b96e5886eb1ed4
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-08 20:52:28 +0600

    added the button

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -361,7 +361,7 @@ export const Home = () => {
                    <div className="flex items-center justify-center gap-4">
                      <div className="w-14 h-14 rounded-full bg-white/20"></div>
 
-//                     <div className="text-left">
+                     <div className="text-left">
 //                       <h4 className="font-bold text-white">
 //                         Rahat Chowdhury
 //                       </h4>
