5de04782c390cb89d5fd52e8b4b3e54059cb1fdc
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-09 08:55:50 +0600

    added the loading

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -388,7 +388,7 @@ export const Home = () => {
              }
 
              100% {
-//               transform: translateX(-100%);
+               transform: translateX(-100%);
 //             }
 //           }
 
