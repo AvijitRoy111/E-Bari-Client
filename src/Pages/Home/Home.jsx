cceda031a4862fa9713ca382899ddd0f7a10f3e2
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-09 09:02:09 +0600

    added the animation

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -395,7 +395,7 @@ export const Home = () => {
            .animate-marquee {
              display: inline-block;
              animation: marquee 25s linear infinite;
-//           }
+           }
 //         `,
 //         }}
 //       />
