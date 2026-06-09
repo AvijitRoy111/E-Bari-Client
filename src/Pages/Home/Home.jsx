c371aa1f1daa4f65f4a035773cc014e5488de8a5
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-09 08:55:14 +0600

    added the modal

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -384,7 +384,7 @@ export const Home = () => {
            __html: `
            @keyframes marquee {
              0% {
-//               transform: translateX(100%);
+               transform: translateX(100%);
 //             }
 
 //             100% {
