a5e9798cecb0fd0ca105ca83ac7f3eb02469662e
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-07-24 20:59:50 +0700

    added the title

--- src/Components/BrowseHome/BrowseHome.jsx
+++ src/Components/BrowseHome/BrowseHome.jsx
@@ -61,7 +61,7 @@
 
              <h3 className="absolute top-3 left-4 text-white text-xl font-semibold">
                  {item?.title}
-//             </h3>
+             </h3>
 
 //             <div className="absolute top-3 right-4 bg-white text-black w-10 h-10 rounded-full flex items-center justify-center font-semibold shadow">
 //                 {item?.sellCount}
