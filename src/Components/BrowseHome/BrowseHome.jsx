cbafcc3676d57718e2cf6fe549b380a7d63751b8
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-07-26 21:51:46 +0700

    added the brouwse

--- src/Components/BrowseHome/BrowseHome.jsx
+++ src/Components/BrowseHome/BrowseHome.jsx
@@ -101,7 +101,7 @@
                      Browse expensive homes
                  </h3>
 
-//                 <p className="text-xl font-normal text-center px-2 md:px-32">
+                 <p className="text-xl font-normal text-center px-2 md:px-32">
 //                     Welcome to Abriculteurs, the family-run agency committed
 //                     to exceptional service. Discover a meticulous presentation
 //                     of this property: professional photos. Trust our expertise
