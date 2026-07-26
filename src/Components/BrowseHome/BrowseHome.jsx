28faeb0f19d60efdf925c3decf0eaa098f4ad584
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-07-26 21:51:10 +0700

    added the handle less

--- src/Components/BrowseHome/BrowseHome.jsx
+++ src/Components/BrowseHome/BrowseHome.jsx
@@ -98,7 +98,7 @@
              {/* Header */}
              <div className="flex flex-col items-center justify-center gap-4 pb-6">
                  <h3 className="text-4xl font-bold">
-//                     Browse expensive homes
+                     Browse expensive homes
 //                 </h3>
 
 //                 <p className="text-xl font-normal text-center px-2 md:px-32">
