161d34d157644bf551a0ae28a3c88cf1e026c426
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-07-28 22:23:14 +0700

    added the buttom

--- src/Components/BrowseHome/BrowseHome.jsx
+++ src/Components/BrowseHome/BrowseHome.jsx
@@ -124,7 +124,7 @@
                                        item={item}
                                    />
                                ))}
-//                 </div>
+                 </div>
 //             </div>
 
 //             {/* Buttons */}
