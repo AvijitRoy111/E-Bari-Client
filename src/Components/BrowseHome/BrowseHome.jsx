3717919b2d1d7eb29d89b4c2fd409a56233210e4
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-07-28 22:18:32 +0700

    added the loading

--- src/Components/BrowseHome/BrowseHome.jsx
+++ src/Components/BrowseHome/BrowseHome.jsx
@@ -112,8 +112,8 @@
              {/* Cards Grid */}
              <div className="w-full max-w-7xl mx-auto px-4 lg:px-20">
                  <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-//                     {loading
-//                         ? Array.from({ length: 3 }).map((_, idx) => (
+                     {loading
+                         ? Array.from({ length: 3 }).map((_, idx) => (
 //                               <SkeletonCard key={idx} />
 //                           ))
 //                         : homes
