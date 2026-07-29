a0fa3c6b6e9f19d761b9fc7da795cade2d4145eb
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-07-29 21:07:38 +0700

    added the error

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -1,6 +1,5 @@
-
-// const ErrorPage = () => {
-//   // Ager page-e ferot jaoar jonno hander function
+const ErrorPage = () => {
+   // Ager page-e ferot jaoar jonno hander function
 //   const handleGoBack = () => {
 //     window.history.back();
 //   };
