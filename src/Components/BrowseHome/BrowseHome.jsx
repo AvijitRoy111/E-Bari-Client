2b2fd6eaca95a2f1b22b50c832c58e88a4fbaa2a
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-07-29 21:05:12 +0700

    added the text

--- src/Components/BrowseHome/BrowseHome.jsx
+++ src/Components/BrowseHome/BrowseHome.jsx
@@ -143,7 +143,7 @@
                          onClick={handleSeeLess}
                          className="px-6 py-2 bg-red-600 text-white rounded-lg shadow"
                      >
-//                         See Less
+                         See Less
 //                     </button>
 //                 )}
 //             </div>
