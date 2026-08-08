a33dd1f9691ed8105a6d855b0a28f49655245470
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-08-08 20:30:45 +0600

    added the button

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -46,8 +46,8 @@ const ErrorPage = () => {
                 className="mr-2 -ml-1 h-5 w-5 text-gray-500" 
                 fill="none" 
                 viewBox="0 0 24 24" 
-//                stroke="currentColor"
-//              >
+                stroke="currentColor"
+              >
 //                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
 //              </svg>
 //              Ager Page-e Fire Jan
