29020afb0dd02332f4f6e59113080377393fcd46
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-08-08 20:31:56 +0600

    added the image

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -55,8 +55,8 @@ const ErrorPage = () => {
 
             {/* Home Button */}
             <a
-//              href="/"
-//              className="inline-flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium rounded-lg text-white bg-blue-600 hover:bg-blue-700 shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-all"
+              href="/"
+              className="inline-flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium rounded-lg text-white bg-blue-600 hover:bg-blue-700 shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-all"
 //            >
 //              Home-e Fire Jan
 //            </a>
