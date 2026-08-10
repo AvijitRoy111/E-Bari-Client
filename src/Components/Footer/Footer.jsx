19a291bbf9a9127c40c48c5a8b84f0045b9b113a
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-08-10 23:49:40 +0600

    added the text

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -43,7 +43,7 @@
            <ul className="space-y-1.5 text-sm">
              <li><a href="/" className="hover:underline">Home</a></li>
              <li><a href="/all-properties" className="hover:underline">All Properties</a></li>
-//             <li><a href="/services" className="hover:underline">Services</a></li>
+             <li><a href="/services" className="hover:underline">Services</a></li>
 //             <li><a href="/contact" className="hover:underline">Contact</a></li>
 //             <li><a href="/about" className="hover:underline">About Us</a></li>
 //           </ul>
