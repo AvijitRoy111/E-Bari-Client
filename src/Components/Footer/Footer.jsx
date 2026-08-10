80f05893bc6645116826d7a0d8f81a2aecc54d4a
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-08-10 23:51:18 +0600

    added the about

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -45,7 +45,7 @@
              <li><a href="/all-properties" className="hover:underline">All Properties</a></li>
              <li><a href="/services" className="hover:underline">Services</a></li>
              <li><a href="/contact" className="hover:underline">Contact</a></li>
-//             <li><a href="/about" className="hover:underline">About Us</a></li>
+             <li><a href="/about" className="hover:underline">About Us</a></li>
 //           </ul>
 //         </div>
 
