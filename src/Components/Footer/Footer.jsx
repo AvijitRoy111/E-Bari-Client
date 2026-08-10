47b5ce57bf633c9bc98e7ee4a480ab16ea5c15f9
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-08-10 23:49:03 +0600

    added the text

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -40,7 +40,7 @@
          {/* Quick Links */}
          <div className="mt-4 md:mt-0">
            <h4 className="font-semibold mb-2">Quick Links</h4>
-//           <ul className="space-y-1.5 text-sm">
+           <ul className="space-y-1.5 text-sm">
 //             <li><a href="/" className="hover:underline">Home</a></li>
 //             <li><a href="/all-properties" className="hover:underline">All Properties</a></li>
 //             <li><a href="/services" className="hover:underline">Services</a></li>
