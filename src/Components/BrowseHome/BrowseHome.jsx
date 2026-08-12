9fa1c998b14bb1c8711533bde2573f9efd373118
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-08-12 21:11:24 +0600

    added the data

--- src/Components/BrowseHome/BrowseHome.jsx
+++ src/Components/BrowseHome/BrowseHome.jsx
@@ -16,7 +16,7 @@
 
               // Top 9 expensive homes
               const expensiveHomes = data
-//                  .sort(
+                  .sort(
 //                      (a, b) =>
 //                          (b.price_max || b.price) -
 //                          (a.price_max || a.price)
