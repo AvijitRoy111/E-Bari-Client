3518b95c5490d069ef00ce44a961b5cb8016faf6
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-08-13 18:14:36 +0600

    added the skleton

--- src/Components/BrowseHome/BrowseHome.jsx
+++ src/Components/BrowseHome/BrowseHome.jsx
@@ -40,7 +40,7 @@
           <div className="max-w-sm relative rounded-xl overflow-hidden shadow-md animate-pulse">
               <div className="w-full h-48 bg-gray-300"></div>
 
-//              <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent"></div>
+              <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent"></div>
 
 //              <div className="absolute top-3 left-4 bg-gray-400 h-6 w-32 rounded"></div>
 
