2029fe3d93b1cd56f8d5f3ffc40d813db568b13b
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-08-13 18:14:44 +0600

    added the skleton

--- src/Components/BrowseHome/BrowseHome.jsx
+++ src/Components/BrowseHome/BrowseHome.jsx
@@ -42,7 +42,7 @@
 
               <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent"></div>
 
-//              <div className="absolute top-3 left-4 bg-gray-400 h-6 w-32 rounded"></div>
+              <div className="absolute top-3 left-4 bg-gray-400 h-6 w-32 rounded"></div>
 
 //              <div className="absolute top-3 right-4 bg-gray-300 w-10 h-10 rounded-full"></div>
 //          </div>
